2e62837ba64528e472b0fbd593fa4b6492bbde15
MzJennie <talkaboutem@gmail.com>
2026-05-28 10:05:06 +0000

    Docker setup complete

--- Server/MuchToDo/internal/config/config.go
+++ Server/MuchToDo/internal/config/config.go
@@ -6,16 +6,16 @@ import (
 
 // Config stores all configuration of the application.
 type Config struct {
-	ServerPort       string `mapstructure:"PORT"`
-	MongoURI         string `mapstructure:"MONGO_URI"`
-	DBName           string `mapstructure:"DB_NAME"`
-	JWTSecretKey     string `mapstructure:"JWT_SECRET_KEY"`
+	ServerPort         string `mapstructure:"PORT"`
+	MongoURI           string `mapstructure:"MONGO_URI"`
+	DBName             string `mapstructure:"DB_NAME"`
+	JWTSecretKey       string `mapstructure:"JWT_SECRET_KEY"`
 	JWTExpirationHours int    `mapstructure:"JWT_EXPIRATION_HOURS"`
-	EnableCache      bool   `mapstructure:"ENABLE_CACHE"`
-	RedisAddr        string `mapstructure:"REDIS_ADDR"`
-	RedisPassword    string `mapstructure:"REDIS_PASSWORD"`
-	LogLevel      string `mapstructure:"LOG_LEVEL"`
-	LogFormat     string `mapstructure:"LOG_FORMAT"`
+	EnableCache        bool   `mapstructure:"ENABLE_CACHE"`
+	RedisAddr          string `mapstructure:"REDIS_ADDR"`
+	RedisPassword      string `mapstructure:"REDIS_PASSWORD"`
+	LogLevel           string `mapstructure:"LOG_LEVEL"`
+	LogFormat          string `mapstructure:"LOG_FORMAT"`
 }
 
 // LoadConfig reads configuration from file or environment variables.
@@ -24,9 +24,19 @@ func LoadConfig(path string) (config Config, err error) {
 	viper.SetConfigName(".env")
 	viper.SetConfigType("env")
 
+	viper.BindEnv("PORT")
+	viper.BindEnv("MONGO_URI")
+	viper.BindEnv("DB_NAME")
+	viper.BindEnv("JWT_SECRET_KEY")
+	viper.BindEnv("JWT_EXPIRATION_HOURS")
+	viper.BindEnv("ENABLE_CACHE")
+	viper.BindEnv("REDIS_ADDR")
+	viper.BindEnv("REDIS_PASSWORD")
+	viper.BindEnv("LOG_LEVEL")
+	viper.BindEnv("LOG_FORMAT")
+
 	viper.AutomaticEnv()
 
-	// Set default values
 	viper.SetDefault("PORT", "8080")
 	viper.SetDefault("ENABLE_CACHE", false)
 	viper.SetDefault("JWT_EXPIRATION_HOURS", 72)
@@ -36,9 +46,9 @@ func LoadConfig(path string) (config Config, err error) {
 		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
 			return
 		}
+		err = nil
 	}
 
 	err = viper.Unmarshal(&config)
 	return
 }
-
